7e12e00e316bd3f3fb1a3e86fbf04e9099975405
Gusmelchor <gustavomg3891@gmail.com>
2024-06-15 14:44:23 -0500

    diseño de la cabecera

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
-
+import cabecera from "@/components/cabecera.vue"
 </script>
 
 <template>
-  
-
+  <cabecera />
   <RouterView />
 </template>
 
